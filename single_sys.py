d08a6c17f722ac54ed7c1415fcbc13530d99186a
haidi <haidi@mail.ustc.edu.cn>
2020-05-07 23:03:43 +0800

    add single_sys

---
+++ single_sys.py
@@ -0,0 +1,12 @@
+from dpdata import LabeledSystem,MultiSystems
+from glob import glob
+"""
+process multi systems
+"""
+try:
+   ls=LabeledSystem('OUTCAR')
+except:
+   print(f)
+
+ms.to_deepmd_raw('deepmd')
+ms.to_deepmd_npy('deepmd')
